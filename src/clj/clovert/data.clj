30e533086fe09f3f5ff68f0872c9fbc178636d59
James Uther <james@lshift.net>
2014-04-24 11:23:23 +0100

    adding back data sets

--- src/clj/clovert/data.clj
+++ src/clj/clovert/data.clj
@@ -12,21 +12,24 @@
 (def files ["movies100", "movies300", "movies500", "movies700"])
 
 (defmulti replace-node :tag)
+
 (defmethod replace-node :rdf:rdf [node]
   (:content node))
+
 (defmethod replace-node :rdf:Description [node]
   {:about (get-in node [:attrs :about])
    :title (some identity
                 (map #(get-in % [:content 0]) (:content node)))
    :peers (filter identity
                   (map #(get-in % [:attrs :rdf:resource]) (:content node)))
-   :mark  (Float/parseFloat
-           (some identity
-                 (map #(get-in % [:attrs :gmp:mark]) (:content node))))
-   :reliability  (Float/parseFloat
-                  (some identity
-                        (map #(get-in % [:attrs :gmp:reliability]) (:content node))))
-   })
+   :results {:average
+             {:mark
+              (Float/parseFloat (some identity
+                                      (map #(get-in % [:attrs :gmp:mark]) (:content node))))
+              :reliability
+              (Float/parseFloat (some identity
+                                      (map #(get-in % [:attrs :gmp:reliability]) (:content node))))}}})
+
 (defmethod replace-node :default [node]
   node)
 
